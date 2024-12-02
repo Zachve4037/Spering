fc86b978919e698ec95b0735617815827bac0ebd
Zachve_4037 <matej.zachveja@gmail.com>
2024-12-02 19:07:57 +0100

    Added html and css along with images

--- myProject/myProject/settings.py
+++ myProject/myProject/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 from pathlib import Path
 import os
 
+from django.contrib import staticfiles
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -69,9 +71,12 @@ TEMPLATES = [
     },
 ]
 # add the static directory to the staticfiles_dirs list
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'myApp/static'),
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 WSGI_APPLICATION = 'myProject.wsgi.application'
 
@@ -121,7 +126,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
