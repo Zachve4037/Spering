0b8d8d1732bd5546046cee13f755ee376aca63b3
Zachve_4037 <matej.zachveja@gmail.com>
2024-12-04 21:03:57 +0100

    read operation added

--- myProject/myApp/views.py
+++ myProject/myApp/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from .models import Post
 
 
-posted = [
-	{'id': 1, 'title': 'First Post', 'content': 'This is the first post'},
-	{'id': 2, 'title': 'Second Post', 'content': 'This is the second post'},
-	{'id': 3, 'title': 'Third Post', 'content': 'This is the third post'},
-]
+#posted = [
+#	{'id': 1, 'title': 'First Post', 'content': 'This is the first post'},
+#	{'id': 2, 'title': 'Second Post', 'content': 'This is the second post'},
+#	{'id': 3, 'title': 'Third Post', 'content': 'This is the third post'},
+#]
 
 def posts(request):
-	posts = Post.objects.all()
+	posted = Post.objects.all()
 	context = {'posts': posted}
 	return render(request, "myApp/posts.html", context)
 
