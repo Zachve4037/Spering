4287420876a90def68e63941ea25617f13d4d2a5
Zachve4037 <matej.zachveja@gmail.com>
2025-01-06 21:30:37 +0100

    idk what i added but i am finished for today

--- myProject/myApp/admin.py
+++ myProject/myApp/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
 from .models import Post, Comment, Genre
-from .views import category
 
 admin.site.register(Post)
 admin.site.register(Comment)
