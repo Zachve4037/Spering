1ce91158aa20ffe61890f58d6c50c3af9959f962
Zachve4037 <matej.zachveja@gmail.com>
2025-01-06 21:20:44 +0100

    finalyzed the entities

--- myProject/myApp/admin.py
+++ myProject/myApp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Comment, Genre
+from .views import category
 
 admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Genre)
--- myProject/myApp/models.py
+++ myProject/myApp/models.py
@@ -1,13 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import SET_NULL
+
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
-    content = models.TextField(null=True, blank = True)
-    upload = models.DateTimeField(auto_now_add=True)
+    author_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(null=False, blank = False)
+    created_at = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
+    genre = models.ForeignKey('Genre', on_delete=SET_NULL, null=True)
 
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='post')
+    content = models.TextField(null=False)
+    author_name = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.content
+
+class Genre(models.Model):
+    name = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
