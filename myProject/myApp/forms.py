afe59dd6260464ad6110b1ad7a8adb4bc55509ef
Zachve4037 <matej.zachveja@gmail.com>
2025-01-10 14:55:04 +0100

    few details about style added

--- myProject/myApp/forms.py
+++ myProject/myApp/forms.py
@@ -1,7 +1,10 @@
 from django.forms import ModelForm
-from .models import Post
+from .models import Post, Genre
+from django import forms
+
 
 class PostForm(ModelForm):
+    genres = forms.ModelMultipleChoiceField(queryset=Genre.objects.all(), widget=forms.CheckboxSelectMultiple)
     class Meta:
         model = Post
         fields = ['title', 'content', 'genres']
--- myProject/myApp/models.py
+++ myProject/myApp/models.py
@@ -3,7 +3,11 @@ from django.contrib.auth.models import User
 from django.db.models import SET_NULL
 
 
-# Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=15, unique=True)
+
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
@@ -11,7 +15,7 @@ class Post(models.Model):
     content = models.TextField(null=False, blank = False)
     created_at = models.DateTimeField(auto_now_add=True)
     update = models.DateTimeField(auto_now=True)
-    genres = models.ManyToManyField('Genre')
+    genres = models.ManyToManyField(Genre)
 
     def __str__(self):
         return self.title
@@ -26,8 +30,3 @@ class Comment(models.Model):
     def __str__(self):
         return self.content
 
-class Genre(models.Model):
-    name = models.CharField(max_length=15, unique=True)
-
-    def __str__(self):
-        return self.name
